38f77ad860a31db3990846eb47fe18e0011ab73c
Yuji Okazawa <yujiorama@gmail.com>
2020-10-01 17:44:17 +0900

    refactor: testcontainers で PostgreSQL 13 を実行するように変更

--- sakila-app-web/src/test/kotlin/org/bitbucket/yujiorama/sakilaapp/endpoint/CountryRepositoryTest.kt
+++ sakila-app-web/src/test/kotlin/org/bitbucket/yujiorama/sakilaapp/endpoint/CountryRepositoryTest.kt
@@ -6,14 +6,43 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.util.TestPropertyValues
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.test.context.ContextConfiguration
+import org.testcontainers.containers.PostgreSQLContainerProvider
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(
+        initializers = [
+            CountryRepositoryTest.TestDatabaseInitializer::class
+        ])
 class CountryRepositoryTest(
         @Autowired
         val restTemplate: TestRestTemplate
 ) {
+
+    class TestDatabaseInitializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
+        override fun initialize(applicationContext: ConfigurableApplicationContext) {
+
+            database.start()
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + database.getJdbcUrl(),
+                    "spring.datasource.username=" + database.getUsername(),
+                    "spring.datasource.password=" + database.getPassword()
+            ).applyTo(applicationContext.environment)
+        }
+
+        companion object {
+            val database = PostgreSQLContainerProvider()
+                    .newInstance("13-alpine")
+                    .withDatabaseName("sakila")
+                    .withUsername("postgres")
+                    .withPassword("postgres")
+        }
+    }
 
     @BeforeAll
     fun setup() {
