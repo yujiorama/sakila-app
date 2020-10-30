fdbb482b593b34d2fa1a97dd418ca84addc143c5
Yuji Okazawa <yujiorama@gmail.com>
2020-10-30 13:30:04 +0900

    fix: ビルダーの使い方の間違いを修正

--- sakila-app-web/src/main/kotlin/org/bitbucket/yujiorama/sakilaapp/AppConfig.kt
+++ sakila-app-web/src/main/kotlin/org/bitbucket/yujiorama/sakilaapp/AppConfig.kt
@@ -1,22 +1,6 @@
 package org.bitbucket.yujiorama.sakilaapp
 
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.time.format.DateTimeFormatter
 
 @Configuration
-class AppConfig {
-    @Bean
-    fun jacksonCustomizer(): Jackson2ObjectMapperBuilderCustomizer {
-
-        return Jackson2ObjectMapperBuilderCustomizer { builder ->
-            run {
-                builder.serializers(LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME))
-                builder.deserializers(LocalDateTimeDeserializer(DateTimeFormatter.ISO_DATE_TIME))
-            }
-        }
-    }
-}
+class AppConfig
--- sakila-app-web/src/main/kotlin/org/bitbucket/yujiorama/sakilaapp/WebMfcConfig.kt
+++ sakila-app-web/src/main/kotlin/org/bitbucket/yujiorama/sakilaapp/WebMfcConfig.kt
@@ -1,15 +1,31 @@
 package org.bitbucket.yujiorama.sakilaapp
 
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Component
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
+import java.time.format.DateTimeFormatter
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-@Component
+@Configuration
 class WebMfcConfig : WebMvcConfigurer {
+
+    @Bean
+    fun jacksonCustomizer(): Jackson2ObjectMapperBuilderCustomizer {
+
+        return Jackson2ObjectMapperBuilderCustomizer { builder ->
+            run {
+                builder.serializers(LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                    .deserializers(LocalDateTimeDeserializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+            }
+        }
+    }
 
     override fun addInterceptors(registry: InterceptorRegistry) {
 
@@ -21,10 +37,14 @@ class LoggingInterceptor : HandlerInterceptorAdapter() {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
 
         logger.info("request: method=[{}], url=[{}]", request.method, request.requestURL)
+        return true
+    }
+
+    override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
+
         logger.info("response: status=[{}], exception=[{}]", response.status, ex)
     }
 }
-
